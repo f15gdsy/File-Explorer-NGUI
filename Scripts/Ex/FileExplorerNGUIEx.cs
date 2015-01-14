3ebf8185fc21ef3a4eea910d0095ee8660b38ad5
TheGameFather <f15gdsy@gmail.com>
2015-01-14 12:39:06 +0800

    - Can open a window by using prefab path.

--- Scripts/Ex/FileExplorerNGUIEx.cs
+++ Scripts/Ex/FileExplorerNGUIEx.cs
@@ -4,7 +4,7 @@ using FileExplorerNGUI.UI;
 
 namespace FileExplorerNGUI.Ex {
 	
-	// FileExplorerEx provides interfaces for other applications to use.
+	// FileExplorerNGUIEx provides interfaces for other applications to use.
 	public class FileExplorerNGUIEx {
 		
 		private static GameObject _windowPrefab;
@@ -12,16 +12,24 @@ namespace FileExplorerNGUI.Ex {
 		
 		
 		public static void Open (WindowControllerNGUI controller, string prefabPath) {
+			if (_windowPrefab == null) {
+				_windowPrefab = Resources.Load(prefabPath) as GameObject;
+			}
+
+			_windowGo = GameObject.Instantiate(_windowPrefab) as GameObject;
+			_windowGo.transform.localPosition = Vector3.zero;		// TODO: leave an interface for positioning the window?
 			
+			WindowBaseNGUI window = _windowGo.GetComponent<WindowBaseNGUI>();
+			window.RegisterWindowController(controller);
 		}
 		
 		// Used to open a File Explorer window.
 		// 	controller: a customized controller to responds to window UI interaction.
 		//	style: wanted window style.
 		public static void Open (WindowControllerNGUI controller, WindowStyle style = WindowStyle.Default) {
+			string prefabPath = "";
+
 			if (_windowPrefab == null) {
-				string prefabPath;
-				
 				switch (style) {
 				case WindowStyle.List:
 					prefabPath = "Prefabs/File Explorer List Window NGUI";
@@ -31,16 +39,9 @@ namespace FileExplorerNGUI.Ex {
 					prefabPath = "Prefabs/File Explorer List Window NGUI";
 					break;
 				}
-				
-				_windowPrefab = Resources.Load(prefabPath) as GameObject;
 			}
 
-			
-			_windowGo = GameObject.Instantiate(_windowPrefab) as GameObject;
-			_windowGo.transform.localPosition = Vector3.zero;		// TODO: leave an interface for positioning the window?
-			
-			WindowBaseNGUI window = _windowGo.GetComponent<WindowBaseNGUI>();
-			window.RegisterWindowController(controller);
+			Open(controller, prefabPath);
 		}
 		
 		// Close the window.
